f0dc7e1fc01b0e09146d4ec993a07d0ecf967536
Revanth <revanthg2607@gmail.com>
2025-01-01 20:17:36 +0530

    code update

--- Hello-World/hardhat.config.js
+++ Hello-World/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: "update your network http here",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
+
--- Hello-World/scripts/deploy.js
+++ Hello-World/scripts/deploy.js
@@ -7,27 +7,19 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const HelloWorld = await ethers.getContractFactory("HelloWorld");
+  console.log("Deploying Contract...")
+  const helloWorld = await HelloWorld.deploy("Hello World! Bingo");
+  const txHash = helloWorld.deployTransaction.hash;
+  const txReceipt = await ethers.provider.waitForTransaction(txHash);
+  console.log("Contract deployed to address:",  txReceipt.contractAddress);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
